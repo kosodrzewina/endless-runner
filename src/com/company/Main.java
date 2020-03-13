57c1c72953190d4af747645e9fcc91ec6b4754f7
Kosodrzewinatru <sienkiew@live.com>
2020-03-13 17:59:09 +0100

    added frame

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,25 @@
 package com.company;
 
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Frame frame = new Frame("Runner");
+
+        frame.setSize(1000, 500);
+        frame.setVisible(true);
+
+        frame.addWindowListener(
+                new WindowAdapter() {
+                    @Override
+                    public void windowClosing(WindowEvent e) {
+                        super.windowClosing(e);
+                        System.exit(0);
+                    }
+                }
+        );
     }
 }
