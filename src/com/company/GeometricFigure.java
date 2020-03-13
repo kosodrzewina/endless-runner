78557237b5ef4f40fff9cf4f8a2a2df1a243c0e1
Kosodrzewinatru <sienkiew@live.com>
2020-03-13 18:30:27 +0100

    added color to GeometricFigure

--- src/com/company/GeometricFigure.java
+++ src/com/company/GeometricFigure.java
@@ -3,5 +3,11 @@ package com.company;
 import java.awt.*;
 
 abstract public class GeometricFigure {
+    Color color;
+
+    public GeometricFigure(Color color) {
+        this.color = color;
+    }
+
     abstract public void paint(Graphics graphics);
 }
