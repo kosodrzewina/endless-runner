e2bbb62dcefa1fa9bd8f48ce604c29be597dcce7
Kosodrzewinatru <sienkiew@live.com>
2020-03-16 20:52:07 +0100

    added Rectangle

---
+++ src/com/company/Rectangle.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.awt.*;
+
+public class Rectangle extends GeometricFigure {
+    int height, width;
+
+    public Rectangle(int height, int width, Color color) {
+        super(color);
+
+        this.height = height;
+        this.width = width;
+    }
+
+    public Rectangle(int height, int width) {
+        super(new Color(
+                (int) (Math.random() * 255),
+                (int) (Math.random() * 255),
+                (int) (Math.random() * 255)
+        ));
+
+        this.height = height;
+        this.width = width;
+    }
+
+    @Override
+    public void paint(Graphics graphics) {
+        graphics.setColor(color);
+        graphics.fillRect(10, 10, width, height);
+    }
+}
