40c8abd24f4b368f85f62556abe5af86888697ba
Kosodrzewinatru <sienkiew@live.com>
2020-03-18 00:42:53 +0100

    added Palette

---
+++ src/com/company/Palette.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.awt.*;
+
+public class Palette {
+    static Color skyDay = Color.decode("#3F51B5");
+    static Color skyNight = Color.black;
+    static Color sun = Color.decode("#FDD835");
+    static Color moon = Color.decode("#C5CAE9");
+}
