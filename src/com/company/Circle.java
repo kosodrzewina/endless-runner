ddfb2c3f8ec4c2bafdcec0e84996b47bce78223f
Kosodrzewinatru <sienkiew@live.com>
2020-03-17 23:55:12 +0100

    added Circle

---
+++ src/com/company/Circle.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.awt.*;
+
+public class Circle extends GeometricFigure {
+    int radius;
+
+    public Circle(int radius, Color color) {
+        super(color);
+
+        this.radius = radius;
+    }
+
+    @Override
+    public void paint(Graphics graphics) {
+        graphics.setColor(color);
+        graphics.fillOval(0, 0, 2 * radius, 2 * radius);
+    }
+}
