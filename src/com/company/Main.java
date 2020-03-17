5f0db15e875c9bcc18fc6d57eba56caa68abb333
Kosodrzewinatru <sienkiew@live.com>
2020-03-18 00:43:15 +0100

    added and implemented setEnvironment function

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,13 +7,30 @@ import java.awt.event.WindowEvent;
 public class Main extends Frame {
 
     public void paint(Graphics graphics) {
+        Color[] environment = setEnvironment(false);
         int groundLevel = getSize().height - 100;
+
+        setBackground(environment[0]);
 
         Ground ground = new Ground(groundLevel);
         ground.paint(graphics);
 
-        Circle circle = new Circle(100, Color.yellow);
-        circle.paint(graphics);
+        Circle sunMoon = new Circle(100, environment[1]);
+        sunMoon.paint(graphics);
+    }
+
+    public Color[] setEnvironment(boolean time) {
+        Color[] colors = new Color[2];
+
+        if (time) {
+            colors[0] = Palette.skyDay;
+            colors[1] = Palette.sun;
+        } else {
+            colors[0] = Palette.skyNight;
+            colors[1] = Palette.moon;
+        }
+
+        return colors;
     }
 
     public Main() {
