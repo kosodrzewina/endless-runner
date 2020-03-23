7640ba8edcc6d3404471b3e526e0248a2d7c470c
Kosodrzewinatru <sienkiew@live.com>
2020-03-23 19:43:45 +0100

    added button for switching mode

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,16 @@
 package com.company;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class Main extends Frame {
+    boolean environmentState = true;
 
     public void paint(Graphics graphics) {
-        Color[] environment = setEnvironment(false);
+        Color[] environment = setEnvironment(environmentState);
         int groundLevel = getSize().height - 100;
 
         setBackground(environment[0]);
@@ -36,6 +39,10 @@ public class Main extends Frame {
     public Main() {
         setTitle("Runner");
 
+        Button timeSwitch = new Button("switch to night");
+        add(timeSwitch);
+
+        setLayout(new FlowLayout());
         setSize(1000, 500);
         setVisible(true);
 
@@ -48,6 +55,16 @@ public class Main extends Frame {
                     }
                 }
         );
+
+        timeSwitch.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                environmentState = !environmentState;
+                repaint();
+
+                timeSwitch.setLabel((environmentState) ? "switch to night" : "switch to day");
+            }
+        });
     }
 
     public static void main(String[] args) {
