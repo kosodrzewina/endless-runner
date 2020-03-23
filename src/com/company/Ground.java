961f6de3fb783d85b365cf29f33a220ea06aad3c
Kosodrzewinatru <sienkiew@live.com>
2020-03-23 17:27:51 +0100

    Ground doesn't extend GeometricFigure anymore

--- src/com/company/Ground.java
+++ src/com/company/Ground.java
@@ -2,17 +2,14 @@ package com.company;
 
 import java.awt.*;
 
-public class Ground extends GeometricFigure {
+public class Ground {
     private int level;
     private static Color color = Color.darkGray;
 
     public Ground(int level) {
-        super(color);
-
         this.level = level;
     }
 
-    @Override
     public void paint(Graphics graphics) {
         graphics.setColor(color);
         graphics.fillRect(0, level, (int) Double.POSITIVE_INFINITY, (int) Double.POSITIVE_INFINITY);
