c0cfa7bdc64b1e3cc02eb97ab28507d0fcc3fb1b
Kosodrzewinatru <sienkiew@live.com>
2020-03-30 22:47:04 +0200

    added Node

---
+++ src/com/company/Node.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Node {
+    Node next;
+    GeometricFigure geometricFigure;
+
+    public Node(GeometricFigure geometricFigure) {
+        this.geometricFigure = geometricFigure;
+    }
+}
