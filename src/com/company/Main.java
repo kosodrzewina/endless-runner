cb5febdb86685cd81a2ec807966cbffd4b94938e
Kosodrzewinatru <sienkiew@live.com>
2020-04-16 17:54:11 +0200

    fixed going out of bound

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -104,8 +104,10 @@ public class Main extends Frame {
         // drawing obstacles on the ground
         for (int i = 0; i < obstacles.size(); i++) {
             // preventing memory leak
-            if (obstacles.getAt(i).x + obstacles.getAt(i).width < 0)
+            if (obstacles.getAt(i).x + obstacles.getAt(i).width < 0) {
                 obstacles.deleteFrom(i);
+                i = 0;
+            }
 
             obstacles.getAt(i).y = groundLevel - obstacles.getAt(i).height;
             obstacles.getAt(i).paint(graphics);
